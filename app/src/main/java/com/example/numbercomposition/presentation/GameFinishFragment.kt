5e5dd28e1e571db5330f95c77a27ec38d12a00bb
unknown <serhiiromanchuk@gmail.com>
2023-03-26 19:18:07 +0300

    implement GameFinishFragment

--- app/src/main/java/com/example/numbercomposition/presentation/GameFinishFragment.kt
+++ app/src/main/java/com/example/numbercomposition/presentation/GameFinishFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
-import com.example.numbercomposition.databinding.FragmentGameBinding
+import com.example.numbercomposition.R
 import com.example.numbercomposition.databinding.FragmentGameFinishBinding
 import com.example.numbercomposition.domain.entity.GameResult
 
@@ -35,11 +35,50 @@ class GameFinishFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bindListeners()
+        bindViews()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun bindViews() {
+        binding.apply {
+            emojiResult.setImageResource(getSmileResId())
+            tvRequiredAnswers.text = String.format(
+                getString(R.string.required_answers),
+                gameResult.gameSettings.minCountOfRightAnswers
+            )
+            tvScoreAnswers.text = String.format(
+                getString(R.string.score_answers),
+                gameResult.countOfRightAnswers
+            )
+            tvRequiredPercentage.text = String.format(
+                getString(R.string.required_percentage),
+                gameResult.gameSettings.minPercentOfRightAnswers
+            )
+            tvScorePercentage.text = String.format(
+                getString(R.string.score_percentage),
+                getPercentOfRightAnswers()
+            )
+        }
+    }
+
+    private fun getSmileResId(): Int {
+        return if (gameResult.winner) {
+            R.drawable.ic_smile
+        } else {
+            R.drawable.ic_sad
+        }
+    }
+
+    private fun getPercentOfRightAnswers() = with(gameResult) {
+        if (countOfQuestion == 0) {
+            0
+        } else {
+            ((countOfRightAnswers / countOfQuestion.toDouble()) * 100).toInt()
+        }
     }
 
     private fun bindListeners() {
@@ -78,6 +117,5 @@ class GameFinishFragment : Fragment() {
                 }
             }
         }
-
     }
 }
